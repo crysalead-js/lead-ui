84320d33e1febb6538ed96ac97448a25835a33e9
Simon JAILLET <sjaillet@gmail.com>
2015-08-27 18:28:19 +0200

    Adds index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var Component = require("./src/component");
+var Components = require("./src/components");
+var Renderer = require("./src/renderer");
+
+module.exports = {
+  Component: Component,
+  Components: Components,
+  Renderer: Renderer
+};
